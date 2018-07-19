58b742b6a9b5ed49a8b572b8843362b168b0d437
Steven Chen <steven.chen313@gmail.com>
2018-07-19 08:51:26 -0700

    Finish phase 1

--- W5D4/hw.js
+++ W5D4/hw.js
@@ -8,13 +8,25 @@ function madLib(verb, adj, noun) {
 madLib('make', 'best', 'guac')
 
 function.isSubstring(searchString, subString) {
-
+  return searchString.includes(subString)
 }
 
 function fizzBuzz(array) {
   var new_arr = [];
   array.forEach(function(el) {
-    if el % 3 === 0 || el % 5 === 0
-    new_arr.push(el);
+    if (el % 3 === 0) ^ (el % 5 === 0) {
+      new_arr.push(el);
+    }
   });
+  return new_arr;
+}
+
+function isPrime (n) {
+  if (n<2) { return false; }
+  for (let i = 2; i < n, i++) {
+    if (n%1 == 0) {
+      return false;
+    }
+  }
+  return true;
 }
