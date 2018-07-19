47dead6274562f6c158d9a91d695ec4f0853258b
Steven Chen <steven.chen313@gmail.com>
2018-07-19 08:46:02 -0700

    Submit W5D4 hw

---
+++ W5D4/hw.js
@@ -0,0 +1,20 @@
+function madLib(verb, adj, noun) {
+  verb.toUpperCase;
+  adj.toUpperCase;
+  noun.toUpperCase;
+  console.log(`We shall ${verb} the ${adj} ${noun}.`);
+}
+
+madLib('make', 'best', 'guac')
+
+function.isSubstring(searchString, subString) {
+
+}
+
+function fizzBuzz(array) {
+  var new_arr = [];
+  array.forEach(function(el) {
+    if el % 3 === 0 || el % 5 === 0
+    new_arr.push(el);
+  });
+}
